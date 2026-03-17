0f7a32efc7a7297e187517b5ba043f33d944aede
Naffin, Kyle <kdnaffin@gmail.com>
2026-03-16 18:56:52 -0700

    Finished Assignment

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -4,10 +4,42 @@
 #include <sstream> // For parsing lines
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string& filename);
+string format(string str);
 
 int main() {
    parseCSV("students.csv");
 
    return 0;
+}
+
+void parseCSV(const string& filename){
+   ifstream file(filename);
+   if (!file.is_open()){
+      cout << "File error";
+      return;
+   }
+   stringstream stream;
+   stream << file.rdbuf();
+   file.close();
+
+   string line;
+
+   while (getline(stream, line)) {
+
+      stringstream student(line);
+      string field;
+      
+      while (getline(student, field, ' ')) {
+         cout << format(field) << endl;
+      };
+      cout << endl;
+   }
+   
+   return;
+}
+
+string format(string str){
+   str.insert(str.find(":") + 1, " ");
+   return str;
 }
